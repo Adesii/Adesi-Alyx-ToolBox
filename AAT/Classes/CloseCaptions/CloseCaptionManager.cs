fddec9680367256efcd77d0bd74f6ff1b466a401
Adesi <kopietz123@gmail.com>
2021-03-24 08:13:39 +0100

    further Work on Closed captions

--- AAT/Classes/CloseCaptions/CloseCaptionManager.cs
+++ AAT/Classes/CloseCaptions/CloseCaptionManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using HLACaptionReplacer;
 
@@ -7,7 +8,19 @@ namespace AAT.Classes.CloseCaptions
 {
     public static class CloseCaptionManager
     {
-        public static List<ClosedCaption> AddonSpecificCaptions = new List<ClosedCaption>();
+        public static List<LanguageCaption> AddonSpecificCaptions = new List<LanguageCaption>();
+
+        public static string CurrLang = "english";
+        public static ObservableCollection<ClosedCaptions> languageSpecifCaptions
+        {
+            get
+            {
+                return new ObservableCollection<ClosedCaptions>((IEnumerable<ClosedCaptions>)AddonSpecificCaptions.Find((e) =>
+                {
+                    return e.language.Equals(CurrLang);
+                }).captions);
+            }
+        }
 
     }
 }
---
+++ AAT/Classes/CloseCaptions/LanguageCaption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HLACaptionReplacer;
+
+namespace AAT.Classes.CloseCaptions
+{
+    public class LanguageCaption
+    {
+        public string language = "N/A";
+        public List<ClosedCaption> captions;
+    }
+}
